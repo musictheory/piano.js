4e0bc9e4d48a4db26e67182e8da5542f6dd8e1e0
Ricci <git@ricciadams.com>
2015-04-29 01:44:12 -0700

    First sounds!

--- piano.js
+++ piano.js
@@ -32,6 +32,7 @@ var sRoot     = this;
 var sOldPiano = sRoot.Piano;
 var sVersion  = "0.0.1";
 var sContext  = null;
+var sSilence  = 0.000001;
 
 
 function Instrument()
@@ -42,6 +43,7 @@ function Instrument()
 
     // Privates
     this._buffer = null;
+    this._masterOffset = 0.0;
     this._performer = new Performer(this);
 }
 
@@ -58,8 +60,31 @@ Instrument.prototype.loadAudioFile = function(path, callback)
         var context = instrument._getContext();
 
         context.decodeAudioData(request.response, function(buffer) {
+            /*
+                All MP3 encoders add a delay to the start of the file
+                (see http://lame.sourceforge.net/tech-FAQ.txt)
+
+                To work around this, start our audio file with a sine wave, and
+                measure the offset of the first sample > -6dB (0.5 dBFS)
+
+                This corresponds to sample ~27-28 in the original wav
+            */
+
+            var data   = buffer.getChannelData(0);
+            var actual = 0;
+
+            for (var i = 0; i < buffer.sampleRate; i++) {
+                if (data[i] > 0.5) {
+                    actual = (i / buffer.sampleRate);
+                    break;
+                }
+            }
+
+            instrument._masterOffset = actual - (27.0 / 22050.0);
             instrument._buffer = buffer;
+
             callback();
+
         }, function(error) {
             callback(new Error(path + " could not be decoded."));
         });
@@ -136,13 +161,14 @@ function Zone()
     this.offset        = 0;     // The offset in seconds from the start of the audio file to the start of the sample
     this.loopStart     = 0;     // The offset in samples from this.offset to the start of the loop
     this.loopEnd       = 0;     // The offset in samples from this.offset to the end of the loop
-    this.decay         = 0;     // The time in seconds to -96dB
+    this.duration      = 0;     // Duration (for one-shot zones)
     this.gain          = 1.0;   // The gain in dbFS to apply
     this.keyRangeStart = 0;
     this.keyRangeEnd   = 0;
     this.pitched       = true;
     this.loops         = false;
     this.filters       = [ ];
+    this.decay         = null;
 }
 
 
@@ -155,7 +181,7 @@ Zone.prototype._loadPreset = function(arr)
     this.offset        = num(1, 0);
     this.loopStart     = num(2, 0);
     this.loopEnd       = num(3, 0);
-    this.decay         = num(4, 0);
+    this.duration      = num(4, 0);
     this.gain          = num(5, 1);
     this.keyRangeStart = num(6, 0);
     this.keyRangeEnd   = num(7, 0);
@@ -171,7 +197,7 @@ Zone.prototype._getPreset = function()
         this.offset    || 0,
         this.loopStart || 0,
         this.loopEnd   || 0,
-        this.decay     || 0,
+        this.duration  || 0,
         this.gain      || 1,
         this.keyRangeStart || 0,
         this.keyRangeEnd   || 0,
@@ -215,66 +241,23 @@ function Performer(instrument)
 }
 
 
-Performer.prototype._makeVoices = function(key, timeOffset)
+Performer.prototype._makeVoices = function(key, timeOffset, duration)
 {
-    var context = this._instrument._getContext();
-    var output  = this._instrument._getOutputNode();
-    var buffer  = this._instrument._buffer;
-    var voices  = this._voices;
-
-    function connect(nodes) {
-        for (var i = 0, length = (nodes.length - 1); i < length; i++) {
-            nodes[i].connect(nodes[i+1]);
-        }
-    }
+    var instrument = this._instrument;
+    var voices     = this._voices;
 
     this._instrument.zones.forEach(function(zone) {
-        console.log(zone);
-
         if (!zone.pitched || (key < zone.keyRangeStart) || (key > zone.keyRangeEnd)) {
             return;
         }
 
-        var sourceNode = context.createBufferSource();
-        var gainNode   = context.createGain();
+        var voice = new Voice(key, timeOffset, instrument, zone);
 
-        var nodes = [ sourceNode ];
-
-        (zone.filters || [ ]).forEach(function(filter) {
-            var filterNode = context.createBiquadFilter();
-
-            filterNode.type            = filter.type;
-            filterNode.frequency.value = filter.frequency;
-            filterNode.detune.value    = filter.detune;
-            filterNode.Q.value         = filter.Q;
-            filterNode.gain.value      = filter.gain;
-
-            nodes.push(filterNode);
-        });
-
-        nodes.push(gainNode);
-
-        connect(nodes);
-
-        sourceNode.connect(gainNode);
-        gainNode.connect(output);
-
-        sourceNode.buffer = buffer;
-
-        sourceNode.start(context.currentTime + timeOffset, zone.offset);
-
-        if (zone.looped) {
-            sourceNode.loopStart = zone.offset + (noise.loopStart / sourceNode.sampleRate);
-            sourceNode.loopEnd   = zone.offset + (noise.loopEnd   / sourceNode.sampleRate);
-            sourceNode.loop      = true;
+        if (duration || zone.duration) {
+            voice.stop(timeOffset + Math.min(duration, zone.duration))
         }
 
-        voices.push({
-            key:    key,
-            zone:   zone,
-            source: sourceNode,
-            gain:   gainNode
-        });
+        voices.push(voice);
     });
 }
 
@@ -333,6 +316,83 @@ Performer.prototype.stop = function(arg0)
 
     // body...
 }
+
+
+function Voice(key, timeOffset, instrument, zone)
+{
+    function connect(nodes) {
+        for (var i = 0, length = (nodes.length - 1); i < length; i++) {
+            nodes[i].connect(nodes[i+1]);
+        }
+    }
+
+    var context    = instrument._getContext();
+    var output     = instrument._getOutputNode();
+    var buffer     = instrument._buffer;
+
+    var sourceNode = context.createBufferSource();
+    var decayNode  = context.createGain();
+    var finalNode  = context.createGain();
+
+    var nodes = [ sourceNode, decayNode ];
+
+    (zone.filters || [ ]).forEach(function(filter) {
+        var filterNode = context.createBiquadFilter();
+
+        filterNode.type            = filter.type;
+        filterNode.frequency.value = filter.frequency;
+        filterNode.detune.value    = filter.detune;
+        filterNode.Q.value         = filter.Q;
+        filterNode.gain.value      = filter.gain;
+
+        nodes.push(filterNode);
+    });
+
+    nodes.push(finalNode);
+
+    connect(nodes);
+    finalNode.connect(output);
+
+    sourceNode.buffer = buffer;
+
+    if (zone.loops) {
+        sourceNode.loopStart = zone.offset + instrument._masterOffset + (zone.loopStart / buffer.sampleRate);
+        sourceNode.loopEnd   = zone.offset + instrument._masterOffset + (zone.loopEnd   / buffer.sampleRate);
+        sourceNode.loop      = true;
+    }
+
+    decayNode.gain.value = zone.gain;
+    if (zone.decay) {
+        var decay0Duration = zone.decay[0];
+        var decay0Target   = zone.decay[1];
+        var decay1Duration = zone.decay[2];
+        var decay1Target   = zone.decay[3];
+
+        var decay0Start = context.currentTime + timeOffset;
+        var decay0End   = decay0Start + decay0Duration;
+
+        decayNode.gain.setValueAtTime(zone.gain, decay0Start);
+        decayNode.gain.exponentialRampToValueAtTime(zone.gain * decay1Target, decay0End);
+
+        // Is this a compound decay, if so, do a linear ramp
+        if (decay1Duration) {
+            decayNode.gain.exponentialRampToValueAtTime(decay1Target, decay0End + decay1Duration);
+        }
+    }
+
+    sourceNode.start(context.currentTime + timeOffset, zone.offset + instrument._masterOffset, zone.duration);
+
+    this._sourceNode = sourceNode;
+    this._finalNode  = finalNode;
+}
+
+
+Voice.prototype.stop = function(when)
+{
+    this._finalNode.setValueAtTime(sSilence)
+    this._finalNode.setValueAtTime(sSilence, context.currentTime + when)
+}
+
 
 
 var Piano = {
